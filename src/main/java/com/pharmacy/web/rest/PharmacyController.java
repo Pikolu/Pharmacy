42df401e94ac7f84e034fc6598f8099e71871036
Alexander Popow <apopow@gmail.com>
2016-03-02 23:19:23 +0100

    Commit

---
+++ src/main/java/com/pharmacy/web/rest/PharmacyController.java
@@ -0,0 +1,36 @@
+package com.pharmacy.web.rest;
+
+import com.pharmacy.domain.Pharmacy;
+import com.pharmacy.domain.SearchResult;
+import com.pharmacy.service.api.PharmacyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.inject.Inject;
+
+/**
+ * Pharmacy GmbH
+ * Created by Alexander on 01.03.2016.
+ */
+@Controller
+public class PharmacyController {
+
+    private final static Logger LOG = LoggerFactory.getLogger(PharmacyController.class);
+
+    @Inject
+    private PharmacyService pharmacyService;
+
+    @RequestMapping(value = "/apotheke/{id}/{pharm}", method = RequestMethod.GET)
+    public ModelAndView displayPharmacy(@PathVariable String id, @PathVariable String pharm) {
+        ModelAndView modelAndView = new ModelAndView("pharmacy");
+        Pharmacy pharmacy = pharmacyService.getPharmacyById(id);
+        modelAndView.addObject("pharmacy", pharmacy);
+        modelAndView.addObject("searchResult", new SearchResult());
+        return modelAndView;
+    }
+}
